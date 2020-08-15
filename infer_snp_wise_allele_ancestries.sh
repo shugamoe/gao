206eff29ccdb1d9c42b471e21af4090992c66b8b
Julian McClellan <juliancmcclellan@gmail.com>
2020-08-15 16:02:45 -0500

    RevealJS presentation rump started. Re-running elai with 10 admixure generations. (new logs to be uploaded.)

--- infer_snp_wise_allele_ancestries.sh
+++ infer_snp_wise_allele_ancestries.sh
@@ -6,11 +6,11 @@ elai -g data/1000CEU_Chr21.geno.txt -p 10 \
   -g data/1000YRI_Chr21.geno.txt -p 11 \
   -g data/CancerChr21.geno.txt -p 1 \ 
   -pos data/CancerChr21.pos.txt -s 30 -C 2 -c 10 \
-  -o Adm_Cancer_Chr21 -mixgen 50 --exclude-nopos --exclude-miss1
+  -o Adm_Cancer_Chr21 -mixgen 10 --exclude-nopos --exclude-miss1
 elai -g data/1000CEU_Chr22.geno.txt -p 10 \
   -g data/1000YRI_Chr22.geno.txt -p 11 \
   -g data/CancerChr22.geno.txt -p 1 \
   -pos data/CancerChr22.pos.txt -s 30 -C 2 -c 10 \
-  -o Adm_Cancer_Chr22 -mixgen 50 --exclude-nopos --exclude-miss1
+  -o Adm_Cancer_Chr22 -mixgen 10 --exclude-nopos --exclude-miss1
 
 # CEU = European | YRI = African
